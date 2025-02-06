272ece5dabd48d68b2bd75b96cf016795fcea2f9
mariamtarek <mariam.tarek@oneorder.net>
2025-02-06 22:31:05 +0200

    refactor: Separate AddToCart logic

--- react-ecommerce/src/components/Products/components/ProductCard/ProductCard.jsx
+++ react-ecommerce/src/components/Products/components/ProductCard/ProductCard.jsx
@@ -1,53 +1,13 @@
-import {useMemo,useState} from 'react'
 import RatingStar from '../RatingStar/RatingStar'
 import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { addToCart, updateQuantity } from '../../../../lib/cartSlice'
+import AddToCart from './components/AddToCart/AddToCart'
 
 export default function ProductCard({product}) {
-  const dispatch = useDispatch()
-  const {cartItems} = useSelector((state)=>state.cart)
-  const [showControls, setShowControls] = useState(false);
-
-  const cartItem = useMemo(() => cartItems.find((item) => item.id === product.id), [cartItems, product.id]);
-  const isInCart = !!cartItem;
-  const quantityInCart = cartItem?.quantity || 0;
-
-  function handleUpdateQuantity(updateFlag){
-    if(updateFlag=='+'){
-      dispatch(updateQuantity({id:product.id, newQuantity:quantityInCart+1}))
-    }else{
-      dispatch(updateQuantity({id:product.id, newQuantity:quantityInCart-1}))
-    }
-  }
   return ( <>
     <div className="relative group flex flex-col gap-3" >
        <div className="overflow-hidden aspect-w-1 aspect-h-1 h-48 md:h-80 md:p-5 relative">
           <img className="w-full h-full transition-all duration-300 hover:scale-115 object-contain" src={product.image} alt={product.title} />
-          <div className='absolute bottom-5 right-5 transition-all duration-500' onMouseEnter={() => setShowControls(true)} onMouseLeave={() => setShowControls(false)}>
-          {
-            isInCart && showControls? 
-            <div className='flex flex-col gap-2 items-center'>
-                <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
-                  onClick={() => handleUpdateQuantity("-")}>
-                  -
-               </button>
-               <span className='bg-white px-2 border border-black'>{ quantityInCart }</span>
-               <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
-                 onClick={() => handleUpdateQuantity('+')}>
-                  +
-               </button>
-            </div>
-            : 
-            <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
-                onClick={() => dispatch(addToCart(product))}>
-                  {
-                    isInCart? <span className='text-sm'>x{quantityInCart}</span> :
-                    '+'
-                  }
-             </button>
-          }
-        </div>
+          <AddToCart product={product}/>
        </div>
        <div className="absolute left-3 top-3">
           <p className="sm:px-3 sm:py-1.5 px-1.5 py-1 text-[8px] sm:text-xs font-bold tracking-wide text-gray-900 uppercase bg-slate-100 rounded-full">{product.category}</p>
---
+++ react-ecommerce/src/components/Products/components/ProductCard/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,50 @@
+import {useMemo,useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { addToCart, updateQuantity } from '../../../../../../lib/cartSlice'
+
+export default function AddToCart({product}) {
+  const dispatch = useDispatch()
+  const {cartItems} = useSelector((state)=>state.cart)
+  const [showControls, setShowControls] = useState(false);
+
+  const cartItem = useMemo(() => cartItems.find((item) => item.id === product.id), [cartItems, product.id]);
+  const isInCart = !!cartItem;
+  const quantityInCart = cartItem?.quantity || 0;
+
+  function handleUpdateQuantity(updateFlag){
+    if(updateFlag=='+'){
+      dispatch(updateQuantity({id:product.id, newQuantity:quantityInCart+1}))
+    }else{
+      dispatch(updateQuantity({id:product.id, newQuantity:quantityInCart-1}))
+    }
+  }
+
+  return (
+    <div className='absolute bottom-5 right-5 transition-all duration-500' onMouseEnter={() => setShowControls(true)} onMouseLeave={() => setShowControls(false)}>
+          {
+            isInCart && showControls? 
+            <div className='flex flex-col gap-2 items-center'>
+                <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
+                  onClick={() => handleUpdateQuantity("-")}>
+                  {quantityInCart == 1 ? <FontAwesomeIcon icon={faTrashAlt} className='text-sm'/> : '-' }
+               </button>
+               <span className='bg-white px-2 border border-black'>{ quantityInCart }</span>
+               <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
+                 onClick={() => handleUpdateQuantity('+')}>
+                  +
+               </button>
+            </div>
+            : 
+            <button className='w-10 h-10 flex items-center justify-center rounded-full font-semibold gap-1 border-2  border-black text-black bg-white text-2xl shadow-2xl hover:cursor-pointer hover:bg-slate-800 hover:border-slate-800 hover:text-white transition-all duration-300'
+                onClick={() => dispatch(addToCart(product))}>
+                  {
+                    isInCart? <span className='text-sm'>x{quantityInCart}</span> :
+                    '+'
+                  }
+             </button>
+          }
+        </div>
+  )
+}
